c5c914084a4a72782453b355d7ddd085d91e2a1d
Theo Chan <chan.theodore1@gmail.com>
2021-01-07 09:45:26 -0800

    status check implemented

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ REACH THE END BEFORE THE MAN GON GETCHU
 
 CHOICES = """
     ----
+    E. Status Check
     Q. OUIT
     ----
     """
@@ -45,7 +46,14 @@ def main():
 
     # Variables
     done = False
-    q
+
+    km_travelled = 0        # 100km is the end
+    agents_distance = -20   # 0 is the end
+    turns = 0
+    tofu = 3                # 3 is max
+    fuel = 50               # max is 50 L
+    hunger = 0
+
     # MAIN LOOP
     while not done:
         # TODO: Check if reached END GAME
@@ -55,14 +63,25 @@ def main():
 
         user_choice = input("What do you want to do?").lower().strip("!,.?")
 
-        if user_choice == "q":
+        if user_choice == "e":
+            print(f"\t---Status Check---")
+            print(f"\tkm travelled:{km_travelled}")
+            print(f"\tFuel remaining: {fuel} L")
+            print(f"\tAgents are {abs(agents_distance)} kms behind.")
+            print(f"\tYou have {tofu} tofu left.")
+            printe(f"\---------\n")
+        elif user_choice == "q":
             done = True
 
-        # TODO: Change the enviroment based on
+        time.sleep(1.5)
+
+        # TODO: Change the environment based on
         #       user choice, and RNG
         # TODO: Random event generator
+
     # Outro
     print("Thanks for playing. Play again soon!")
 
-if __name__== "__main__":
+
+if __name__ == "__main__":
     main()
